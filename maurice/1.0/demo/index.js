29296d610ee5fa78d1cd4318f14fbbbfaf02beb1
AMacaroon <65013826+AMacaroon@users.noreply.github.com>
2020-08-27 09:25:21 +0100

    Update index.js

--- maurice/1.0/demo/index.js
+++ maurice/1.0/demo/index.js
@@ -1,9 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
     const inputField = document.getElementById("input")
-    inputField.addEventListener("keydown", function(e) {
+    inputField.addEventListener("keydown", fname(e) {
         if (e.code === "Enter") {
       let input = inputField.value;
       console.log(`I typed '${input}'`)
     }
   });
 });
+function fname () {
+
+//remove all characters except word characters, space, and digits
+  let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+
+// 'tell me a story' -> 'tell me story'
+// 'i feel happy' -> 'happy'
+  text = text
+    .replace(/ a /g, " ")
+    .replace(/i feel /g, "")
+    .replace(/whats/g, "what is")
+    .replace(/please /g, "")
+    .replace(/ please/g, "");
+}
